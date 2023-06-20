6933bbbd71a5e6740eaa434263b896bf2a46c64c
Joshua Lee <joshua.lee@obigo.com>
2023-06-20 16:48:15 +0900

    feat: 호스팅 테스트

--- build/static/js/795.371bec21.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkstudy_zoom_sdk=self.webpackChunkstudy_zoom_sdk||[]).push([[795],{5795:function(s,e,n){n.r(e);var r=n(7689),u=n(184);e.default=function(){var s=(0,r.lk)();return(0,u.jsxs)("section",{children:[(0,u.jsx)("h1",{children:"Error Boundary"}),(0,u.jsx)("small",{children:null===s||void 0===s?void 0:s.message})]})}}}]);
-//# sourceMappingURL=795.371bec21.chunk.js.map
